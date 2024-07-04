be5c1c6a494a94f0c74c414a78645267afcc87d2
Fatiralfatih <fatiralfath48@gmail.com>
2024-07-04 14:52:15 +0700

    feat: add mock api elements and weapons for whuteirng waves

---
+++ src/features/whuteringWaves/utils/mockApi.ts
@@ -0,0 +1,55 @@
+
+const elementsWhutering = {
+    aero: {
+        title: 'Aero',
+        thumbnail: 'https://rerollcdn.com/WW/ww-images/icons/T_IconElementAero.webp',
+    },
+    electro: {
+        title: 'Electro',
+        thumbnail: 'https://rerollcdn.com/WW/ww-images/icons/T_IconElementElectro.webp'
+    },
+    fusion: {
+        title: 'Fusion',
+        thumbnail: 'https://rerollcdn.com/WW/ww-images/icons/T_IconElementFusion.webp',
+    },
+    glacio: {
+        title: 'Glacio',
+        thumbnail: 'https://rerollcdn.com/WW/ww-images/icons/T_IconElementGlacio.webp'
+    },
+    havoc: {
+        title: 'Havoc',
+        thumbnail: 'https://rerollcdn.com/WW/ww-images/icons/T_IconElementHavoc.webp'
+    },
+    spectro: {
+        title: 'Spectro',
+        thumbnail: 'https://rerollcdn.com/WW/ww-images/icons/T_IconElementSpectro.webp'
+    }
+}
+
+const weaponsWhutering = {
+    broadBlade: {
+        title: 'Broad Blade',
+        thumbnail: 'https://rerollcdn.com/WW/ww-images/icons/broadblade.webp'
+    },
+    gauntlets: {
+        title: 'gauntlets',
+        thumbnail: 'https://rerollcdn.com/WW/ww-images/icons/gauntlets.webp'
+    },
+    pistols: {
+        title: 'Pistols',
+        thumbnail: 'https://rerollcdn.com/WW/ww-images/icons/pistols.webp'
+    },
+    rectifier: {
+        title: 'rectifier',
+        thumbnail: 'https://rerollcdn.com/WW/ww-images/icons/rectifier.webp'
+    },
+    sword: {
+        title: 'Sword',
+        thumbnail: 'https://rerollcdn.com/WW/ww-images/icons/sword.webp'
+    }
+}
+
+export {
+    weaponsWhutering,
+    elementsWhutering,
+}
